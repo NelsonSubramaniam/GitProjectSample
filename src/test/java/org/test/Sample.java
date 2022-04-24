bc760ae9a6001752db2e5e750533065611d642ed
NelsonSubramaniam <snb0790@gmail.com>
2022-04-24 15:45:33 +0530

    My prject modification from Nelson

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -6,6 +6,7 @@ public class Sample {
 		
 		System.out.println("My first project creation without master");
 		System.out.println("My first project modification without master");
+		System.out.println("My project modification from Nelson");
 		
 	}
 
