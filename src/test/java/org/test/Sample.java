db417af8240bdb9195b5f70bb7497701083b60d9
NelsonSubramaniam <snb0790@gmail.com>
2022-04-24 15:20:01 +0530

    My first project modification

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("My first project creation without master");
+		System.out.println("My first project modification without master");
 		
 	}
 
